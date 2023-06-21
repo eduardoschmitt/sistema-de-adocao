bf5ae10d597060bc036be407fab7784970c2bc23
Leonardo Martins <leomartinsgc@gmail.com>
2023-06-20 21:35:38 -0300

    dump atualizado

--- bd/dumps/Dump20230620.sql
+++ bd/dumps/Dump20230620.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `sysadocao` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `sysadocao`;
 -- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: sysadocao
@@ -327,4 +329,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-20 20:25:56
+-- Dump completed on 2023-06-20 21:35:00
